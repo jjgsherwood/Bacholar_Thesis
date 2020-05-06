be4eb99756734e0311197b1dae6bbf05aff05c65
Jonne Goedhart <jonnegoedhart@gmail.com>
2020-05-06 14:03:30 +0200

    m

--- flows/run_experiments.sh
+++ flows/run_experiments.sh
@@ -1,5 +1,3 @@
 # !/bin/bash
 
-MPLBACKEND=agg python main.py --k 32 --l 4 --hidden_ch 128 --warmup_steps 10000 --init_lr 0.001 \
-                              --batch_size 64 --epochs 100 --cuda True --log_step 100 \
-                              --dataset mnist --data_dir /storage/datasets/ \
+MPLBACKEND=agg python main.py --k 8 --l 2 --hidden_ch 64 --warmup_steps 10000 --init_lr 0.001 --batch_size 64 --epochs 100 --cuda True --log_step 100 --dataset mnist --data_dir ../../data/
