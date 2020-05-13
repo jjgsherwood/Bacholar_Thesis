f176fe7780d52e3ea3877ab1cbc52ea4518dfe1e
Jonne Goedhart <jonnegoedhart@gmail.com>
2020-05-13 16:34:33 +0200

    m

--- modules/nets.py
+++ modules/nets.py
@@ -43,3 +43,14 @@ def net_liver_Real_NVP(in_channels, hidden_channels):
         nn2.OnlyTensor(norm.Norm(hidden_channels)),
         nn.ReLU(True),
     )
+
+def net_liver_large(in_channels, hidden_channels):
+    return nn.Sequential(
+        nn.Conv3d(in_channels, hidden_channels, (1,1,7), padding=(0,0,3), bias=False),
+        nn2.OnlyTensor(norm.Norm(hidden_channels)),
+        nn.ReLU(True),
+        nn.Conv3d(hidden_channels, hidden_channels, (1,1,5), padding=(0,0,2), bias=False),
+        nn2.OnlyTensor(norm.Norm(hidden_channels)),
+        nn.ReLU(True),
+        nn.Conv3d(hidden_channels, in_channels, kernel_size=1, padding=0, bias=False)
+    )
