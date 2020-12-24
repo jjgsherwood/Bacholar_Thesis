5bb86db94e1989348e73af01cb0ce96e38a19656
Jonne Goedhart <jonne.goedhart@gmail.com>
2020-12-24 12:46:04 +0100

    m

--- Vassilis_work/model.py
+++ Vassilis_work/model.py
@@ -14,6 +14,7 @@ class VAEModel(nn.Module):
             z_dim - Dimensionality of latent space
         """
         super().__init__()
+        print(kwargs)
         for k,v in kwargs:
             setattr(self, k, v)
 
